876183ffb3c35862caf03aa763c2e8e360b4ecfb
구잘 <dksgusals99@gmail.com>
2024-12-07 22:01:05 +0900

    .

--- scriptInf.d.ts
+++ scriptInf.d.ts
@@ -139,13 +139,68 @@ export interface ScriptTcpClient {
 }
 
 export interface ScriptPcan {
-
+    open(channel: number, baudrate: number, payloadBitrate: number, busOffAutoReset: boolean, powerSupply: boolean): boolean;
+    close(): void;
+    setFilter(filterExtended: boolean, filterFrom: number, filterTo: number): boolean;
+    sendCanMessage(type: number, canld: number, data: number[]): boolean;
+    isConnected(): boolean;
+    getStatusString(): string;
+    getCurrentStatus(): number;
+    getCanParameter(parameter: number): number[];
+    setCanParameter(parameter: number, data: number): boolean;
+    canMessagesReceivedSignal: Connectable<(types: number[], messagelds: number[], timestamps: number[], data: number[][]) => void>;
 }
 
 export interface ScriptSerialPort {
-
+    setPortName(name: string): void;
+    portName(): string;
+    setBaudRate(baudRate: number): boolean;
+    baudRate(): number;
+    setDataBits(dataBits: number): boolean;
+    dataBits(): number;
+    setParity(parityString: string): boolean;
+    parity(): string;
+    setStopBits(stopBitsString: string): boolean;
+    stopBits(): string;
+    setFlowControl(flowString: string): boolean;
+    flowControl(): string;
+    errorString(): string;
+    open(): boolean;
+    close(): void;
+    setDTR(set: boolean): void;
+    setRTS(set: boolean): void;
+    isOpen(): boolean;
+    bytesAvailable(): number;
+    readAll(): number[];
+    write(data: number[]): number;
+    writeString(string: string): number;
+    bytesToWrite(): number;
+    waitForBytesWritten(msecs: number): boolean;
+    enableMainInterfaceRouting(): void;
+    disableMainInterfaceRouting(): void;
+    canReadLine(): boolean;
+    readLine(removeNewLine?: boolean, removeCarriageReturn?: boolean): string;
+    readAllLines(removeNewLine?: boolean, removeCarriageReturn?: boolean): string[];
+    readyReadSignal: Connectable<() => void>;
+    getSerialPortSignals(): number;
 }
 
 export interface ScriptAardvarkI2cSpi {
-
+    getInterfaceSettings(): AardvarkI2cSpiSettings
+    i2cMasterFreeBus(): void
+    detectDevices(): string
+    connectToDevice(aardvarkI2cSpiSettings: AardvarkI2cSpiSettings): boolean
+    disconnect(): void
+    i2cMasterReadWrite(flags: number, slaveAddress: number, numberOfBytesToRead: number, dataToSend?: number[]): boolean
+    i2cMasterReadLastReceivedData(): number[]
+    spiMasterSendReceiveData(dataToSend: number[]): boolean
+    spiMasterReadLastReceivedData(): number[]
+    setOutput(pinIndex: number, high: boolean): boolean
+    changePinConfiguration(pinIndex: number, isInput: boolean, withPullups?: boolean): boolean
+    readAllInputs(): boolean[]
+    isConnected(): boolean
+    slaveSetResponse(response: number[]): boolean
+    aardvarkl2cSpilnputStatesChangedSignal(states: boolean[]): void
+    slaveDataSentSignal(data: number[]): void
+    slaveDataReceivedSignal(data: number[]): void
 }
